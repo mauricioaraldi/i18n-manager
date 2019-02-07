7717b374ae6ca8e2a5914ba1c83dc99fc8ee8d58
Mauricio Araldi <araldi.mauricio@gmail.com>
2019-02-07 15:59:39 -0200

    Working with simple objecys

--- assets/main.js
+++ assets/main.js
@@ -104,55 +104,9 @@ function drawLoadedLocales(locales) {
  * @return {String} Message with the sort error encountered
  */
 function verifySorting(data) {
-	let map = mapObjectFromStringArray(data);
-
-	// curIdentationKeys.sort().forEach(key => {
-	// 	console.log('key', key);
-	// 	console.log('data', map);
-	// 	for (let i = map[key].start; i >= map[key].end; i++) {
-	// 		newData.push(data[i]);
-	// 	}
-	// });
-
-	console.log(map);
-	console.log('- - - - - - -');
-
-
-	// let strings = [],
-		// objects = [];
-
-	// for (let key in object) {
-	// 	let data = object[key];
-
-	// 	if (typeof data == 'string') {
-	// 		strings.push(key);
-	// 	} else {
-	// 		objects.push(key);
-	// 		object[key] = verifySorting(data, `${currentKey}${currentKey ? '.' : ''}${key}`);
-	// 	}
-	// }
-
-	// if (!strings.length && !objects.length) {
-	// 	return;
-	// }
-
-	// console.log('- - - - - - - -', currentKey, '- - - - - - - -');
-	// console.log('Strings', strings);
-	// console.log('Objects', objects);
-}
-
-/**
- * Creates a map of the object, returnin the keys, where they start and where they end
- * 
- * @author mauricio.araldi
- * @since 0.0.1
- *
- * @param {Array} data Array with lines representing the object to be mapped
- * @return {Object} The mapped object data
- */
-function mapObjectFromStringArray(data) {
 	let map = {},
-		identationOrder = [];
+		identationOrder = [],
+		sortedData;
 
 	data.forEach((line, index) => {
 		if (line.indexOf('{') > -1 || line.indexOf(':') > -1) {
@@ -175,12 +129,123 @@ function mapObjectFromStringArray(data) {
 			}
 		}
 
-		console.log('map', map);
-
 		if (line.indexOf('}') > -1) {
-			map[identationOrder.pop()].end = index;
+			let key = identationOrder.pop();
+			
+			map[key].end = index;
+
+			sortedData = sortLinesArray(data, map[key].start + 1, map[key].end);
 		}
 	});
 
-	return map;
+	printDiff(createLinesArrayDiff(data, sortedData));
+}
+
+/**
+ * Prints a diff in screen
+ * 
+ * @author mauricio.araldi
+ * @since 0.0.1
+ * 
+ * @param {Array<String>} diff [description]
+ * @return {[type]} [description]
+ */
+function printDiff(diff) {
+	let messageContainer = document.querySelector('#message');
+
+	clearMessageContainer();
+
+	diff.forEach(entry => {
+		let entryEl = document.createElement('SPAN');
+
+		entryEl.textContent = entry;
+
+		messageContainer.appendChild(entryEl);
+	});
+}
+
+/**
+ * Removes all content from message container
+ * 
+ * @author mauricio.araldi
+ * @since 0.0.1
+ */
+function clearMessageContainer() {
+	let messageContainer = document.querySelector('#message');
+
+	// Removes all current children from container
+	while (messageContainer.firstChild) {
+		messageContainer.removeChild(messageContainer.firstChild);
+	}
+}
+
+
+/**
+ * Creates a diff between two array os lines
+ * 
+ * @author mauricio.araldi
+ * @since 0.0.1
+ *
+ * @param {Array<String>} base First array to be used in comparison
+ * @param {Array<String>} comparison Second array to be used in comparison
+ * @return {Array<String>} Array with all the diffs encountered
+ */
+function createLinesArrayDiff(base, comparison) {
+	let numberOfLines = base.length > comparison.length ? base.length : comparison.length,
+		diff = [];
+
+	for (let i = 0; i <= numberOfLines; i++) {
+		if (base[i] != comparison[i]) {
+			diff.push(`Line ${i}:\n[-]${base[i]}\n[+]${comparison[i]}`);
+		}
+	}
+
+	return diff;
+}
+
+/**
+ * Sort an array of lines
+ * 
+ * @author mauricio.araldi
+ * @since 0.0.1
+ *
+ * @param {Array<String>} data The data to be modified (normally the general array, with all lines)
+ * @param {Integer} start Index from which to start sorting
+ * @param {Integer} end Index untill which to sort
+ * 
+ * @return {Array} Modified data, sorted from index start to index end
+ */
+function sortLinesArray(data, start, end) {
+	let sortedData = data.slice(),
+		dataToSort = data.slice(start, end);
+
+	console.log('to sort', sortedData.toString());
+
+	dataToSort.sort((a, b) => {
+		let aKey = a.slice(a.indexOf('"'), a.lastIndexOf('"')),
+			bKey = b.slice(b.indexOf('"'), b.lastIndexOf('"')),
+			keys = [aKey, bKey];
+
+		keys.sort();
+
+		if (aKey === bKey) {
+			return 0;
+		}
+
+		if (keys[0] === bKey) {
+			return 1;
+		}
+
+		if (keys[0] === aKey) {
+			return -1;
+		}
+	});
+
+	for (let i = 0; i < dataToSort.length; i++) {
+		sortedData[start + i] = dataToSort[i];
+	}
+
+	console.log('sorted', sortedData);
+
+	return sortedData;
 }
